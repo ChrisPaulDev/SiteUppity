95f0d021b0af3535129a3d0fca0709c1ddb01efb
Chris Paul <chrispaul.ws@gmail.com>
2015-09-23 01:45:18 -0400

    Add exception handling

--- app.py
+++ app.py
@@ -20,22 +20,28 @@ def index():
 @application.route('/getstatus/<host>/')
 @application.route('/getstatus/<host>')
 def getstatus(host, path=""):
-    path = "/" + path
-    conn = httplib.HTTPConnection(host, 80)
-    conn.request("HEAD", path)
-    res = conn.getresponse()
-    conn2 = httplib.HTTPSConnection(host, 443)
-    conn2.request("HEAD", path)
-    res2 = conn2.getresponse()
-    json1 = "{0} {1}".format(res.status, res.reason)
-    json2 = "{0} {1}".format(res2.status, res2.reason)
-    return '{"http":"' + json1 + '","https":"' + json2 + '"}'
+    try:
+        path = "/" + path
+        conn = httplib.HTTPConnection(host, 80)
+        conn.request("HEAD", path)
+        res = conn.getresponse()
+        conn2 = httplib.HTTPSConnection(host, 443)
+        conn2.request("HEAD", path)
+        res2 = conn2.getresponse()
+        json1 = "{0} {1}".format(res.status, res.reason)
+        json2 = "{0} {1}".format(res2.status, res2.reason)
+        return '{"http":"' + json1 + '","https":"' + json2 + '"}'
+    except:
+        return '{"http":"SiteUppity Error","https":"SiteUppity Error"}'
 
 @application.route('/dig/<d>')
 def dig(d):
-    domain = "{0}".format(d)
-    r = subprocess.check_output(["dig", domain]).splitlines()
-    return json.dumps(r)
+    try:
+        domain = "{0}".format(d)
+        r = subprocess.check_output(["dig", domain]).splitlines()
+        return json.dumps(r)
+    except:
+        return '{"Error":"Error"}'
 
 @application.route('/ping/<domain>')
 def ping(domain):
@@ -43,9 +49,10 @@ def ping(domain):
         domain_str = "{0}".format(domain)
         r = subprocess.check_output(["ping", "-c", "1", domain_str]).splitlines()
         return json.dumps(r)
-    except subprocess.CalledProcessError, e:
-        return '["error"]'
+    except:
+        return '["SiteUppity Error"]'
 
 if __name__ == '__main__':
     application.run(host='127.0.0.1', port='8080')
+
 
