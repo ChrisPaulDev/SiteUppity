2d37164fe8e42c488f5064eef7155e4d759b0dd0
Chris Paul <chrispaul.ws@gmail.com>
2015-09-23 03:10:14 -0400

    Fix up ping

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ from bottle import Bottle, route, view, run, template, TEMPLATE_PATH
 
 TEMPLATE_PATH.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-import subprocess
-import json
-import httplib
+import subprocess, re, json, httplib
 
 application = Bottle()
 
@@ -20,37 +18,48 @@ def index():
 @application.route('/getstatus/<host>/')
 @application.route('/getstatus/<host>')
 def getstatus(host, path=""):
+    json1 = "{0} {1}"
+    json2 = "{0} {1}"
+    path = "/" + path
     try:
-        path = "/" + path
         conn = httplib.HTTPConnection(host, 80)
         conn.request("HEAD", path)
         res = conn.getresponse()
+        json1 = json1.format(res.status, res.reason)
+    except Exception as e:
+        json1 = e.strerror
+
+    try:
         conn2 = httplib.HTTPSConnection(host, 443)
         conn2.request("HEAD", path)
         res2 = conn2.getresponse()
-        json1 = "{0} {1}".format(res.status, res.reason)
-        json2 = "{0} {1}".format(res2.status, res2.reason)
-        return '{"http":"' + json1 + '","https":"' + json2 + '"}'
-    except:
-        return '{"http":"SiteUppity Error","https":"SiteUppity Error"}'
+        json2 = json2.format(res2.status, res2.reason)
+    except Exception as e:
+        json2 = e.strerror        
+
+    return '{"http":"' + json1 + '","https":"' + json2 + '"}'
 
 @application.route('/dig/<d>')
 def dig(d):
     try:
-        domain = "{0}".format(d)
+        domain = str(d)
         r = subprocess.check_output(["dig", domain]).splitlines()
         return json.dumps(r)
-    except:
-        return '{"Error":"Error"}'
+    except Exception as e:
+        return '{"SiteUppity Error":"' + e.strerror + '"}'
 
-@application.route('/ping/<domain>')
-def ping(domain):
+@application.route('/ping/<d>')
+def ping(d):
     try:
-        domain_str = "{0}".format(domain)
-        r = subprocess.check_output(["ping", "-c", "1", domain_str]).splitlines()
-        return json.dumps(r)
-    except:
-        return '["SiteUppity Error"]'
+        domain = str(d)
+        ping = subprocess.Popen(["ping", "-n", "-c 1", domain], stdout=subprocess.PIPE, stderr=subprocess.PIPE)        
+        output = ping.communicate()
+        if output:
+            return json.dumps(output[0].splitlines())
+        else:
+            return '["SiteUppity Error: Could not get ping"]'
+    except subprocess.CalledProcessError:
+        return '["SiteUppity Error: Could not get ping"]'
 
 if __name__ == '__main__':
     application.run(host='127.0.0.1', port='8080')
